03fdc430abbc85595bd04ad9a2ec969224fa2b36
Minot1 <dincer.taner123@gmail.com>
2021-11-22 18:27:08 +0300

    added login-view

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:syboard/routes/welcome.dart';
+import 'package:syboard/routes/login.dart';
 
 void main() => runApp(MaterialApp(
   //home: Welcome(),
   //initialRoute: '/login',
   routes: {
     '/': (context) => Welcome(),
-    // '/login': (context) => Login(),
+    '/login': (context) => Login(),
     // '/signup': (context) => SignUp(),
   },
 ));
