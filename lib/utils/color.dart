e613dd36f22dd064e2ffde397cff4bffb1791945
Minot1 <dincer.taner123@gmail.com>
2021-11-23 16:57:36 +0300

    fixed color lightText

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -4,7 +4,7 @@ class AppColors {
   static const Color primary = const Color(0xFF004890);
   static const Color secondary = const Color(0xFF2A7CC9);
 
-  static const Color lightTextColor = const Color(0x6A6A6A);
+  static const Color lightTextColor = const Color(0xFF6A6A6A);
   static const Color headingColor = const Color(0xFF004890);
   static const Color captionColor = Colors.white30;
 }
