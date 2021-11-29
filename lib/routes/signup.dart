aac1b5e5f629476db445ab2212e92fa28b482aea
EgeAtaCeylan <eata@sabanciuniv.edu>
2021-11-29 22:56:45 +0300

    SIGNUP-CLASS-AND-VIEW
    Ege Ata Ceylan- CS310 Project signup view

---
+++ lib/routes/signup.dart
@@ -0,0 +1,284 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:syboard/utils/color.dart';
+import 'package:syboard/utils/dimension.dart';
+import 'package:syboard/utils/styles.dart';
+import 'package:email_validator/email_validator.dart';
+
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final _formKey = GlobalKey<FormState>();
+  String mail = "";
+  String initialPass = "";
+  String finalPass = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        title: Text(
+          'Sign Up',
+          style: kAppBarTitleTextStyle,
+        ),
+        backgroundColor: AppColors.primary,
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          maintainBottomViewPadding: false,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(
+                height: 96,
+              ),
+              Center(
+                child: Padding(
+                  padding: Dimen.regularPadding,
+                  child: RichText(
+                    text: TextSpan(
+                      text: "Syboard",
+                      style: kLogoTextStyle,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 64,
+              ),
+              Padding(
+                padding: Dimen.regularPadding,
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: TextFormField(
+                                decoration: const InputDecoration(
+                                  hintText: 'E-mail',
+                                  border: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: AppColors.primary,
+                                    ),
+                                    borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                                  ),
+                                  prefixIcon: Icon(Icons.email),
+                                ),
+                                keyboardType: TextInputType.emailAddress,
+                                validator: (value) {
+                                  if (value == null) {
+                                    return 'E-mail field cannot be empty';
+                                  } else {
+                                    String trimmedValue = value.trim();
+                                    if (trimmedValue.isEmpty) {
+                                      return 'E-mail field cannot be empty';
+                                    }
+                                    if (!EmailValidator.validate(
+                                        trimmedValue)) {
+                                      return 'Please enter a valid email';
+                                    }
+                                  }
+                                  return null;
+                                },
+                                onSaved: (value) {
+                                  if (value != null) mail = value;
+                                }),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                hintText: 'Password',
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: AppColors.primary,
+                                  ),
+                                  borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                                ),
+                                prefixIcon: Icon(Icons.lock),
+                              ),
+                              keyboardType: TextInputType.text,
+                              obscureText: true,
+                              enableSuggestions: false,
+                              autocorrect: false,
+                              validator: (value) {
+                                initialPass = value!;
+                                if (value == null) {
+                                  return 'Password field cannot be empty';
+                                } else {
+                                  String trimmedValue = value.trim();
+                                  if (trimmedValue.isEmpty) {
+                                    return 'Password field cannot be empty';
+                                  }
+                                  if (trimmedValue.length < 8) {
+                                    return 'Password must be at least 8 characters long';
+                                  }
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                if (value != null) {
+                                  initialPass = value;
+                                }
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                hintText: 'Confirm Password',
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: AppColors.primary,
+                                  ),
+                                  borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                                ),
+                                prefixIcon: Icon(Icons.lock),
+                              ),
+                              keyboardType: TextInputType.text,
+                              obscureText: true,
+                              enableSuggestions: false,
+                              autocorrect: false,
+                              validator: (value) {
+                                if (value == null) {
+                                  return 'Password field cannot be empty';
+                                } else {
+                                  String trimmedValue = value.trim();
+                                  if (trimmedValue.isEmpty) {
+                                    return 'Password field cannot be empty';
+                                  }
+                                  if (trimmedValue.length < 8) {
+                                    return 'Password must be at least 8 characters long';
+                                  }
+                                  if (value != initialPass) {
+                                    return 'Passwords are not matching';
+                                  }
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                if (value != null) {
+                                  finalPass = value;
+                                }
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Expanded(
+                            flex: 1,
+                            child: OutlinedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  _formKey.currentState!.save();
+                                }
+                              },
+                              child: Padding(
+                                padding:
+                                const EdgeInsets.symmetric(vertical: 12.0),
+                                child: Text(
+                                  'Sign Up',
+                                  style: kButtonDarkTextStyle,
+                                ),
+                              ),
+                              style: OutlinedButton.styleFrom(
+                                backgroundColor: AppColors.primary,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                              flex: 2,
+                              child: SignInButton(
+                                Buttons.Facebook,
+                                text: 'Sign Up with Google',
+                                onPressed: () {},
+                              )),
+                          SizedBox(
+                            width: 8.0,
+                          ),
+                          Expanded(
+                              flex: 2,
+                              child: SignInButton(
+                                Buttons.Google,
+                                text: 'Sign Up with Google',
+                                onPressed: () {},
+                              ))
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              Navigator.popAndPushNamed(context, "/login");
+                            },
+                            child: const Text(
+                              "If you have account? Log In Here",
+                              style: TextStyle(
+                                color: AppColors.primary,
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
