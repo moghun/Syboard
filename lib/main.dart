135b69c304695855337e2fcfb60a0c1ede75c311
Muhammed Orhun Gale <89805772+moghun@users.noreply.github.com>
2021-11-30 10:59:55 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:syboard/routes/welcome.dart';
 import 'package:syboard/routes/login.dart';
+import 'package:syboard/routes/walkthrough.dart'
 
 void main() => runApp(MaterialApp(
   //home: Welcome(),
