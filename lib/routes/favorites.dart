55ada2db430d83da69af6fa72ed415e827f735af
dogkansaracedu <dogkansarac@sabanciuniv.edu>
2021-12-06 21:31:05 +0300

    Router added. Pages initialized

---
+++ lib/routes/favorites.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Favorites extends StatelessWidget {
+  const Favorites({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Favorites"),
+    );
+  }
+}
