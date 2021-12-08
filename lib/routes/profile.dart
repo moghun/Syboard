0a6cf83d3edc9765c86477e4828c4ed76d105a8f
Dogkan Sarac <dogkansarac@sabanciuniv.edu>
2021-12-08 13:57:17 +0300

    list items added to profile page

--- lib/routes/profile.dart
+++ lib/routes/profile.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:syboard/utils/styles.dart';
 import 'package:syboard/utils/color.dart';
+import 'package:syboard/models/account_list_item.dart';
+import 'package:syboard/ui/account_list_item_tile.dart';
+
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
 
@@ -9,6 +12,24 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  static final _itemList = <AccountListItem>[
+    AccountListItem(
+        icon: Icon(Icons.local_shipping_outlined,
+            size: 30, color: Colors.black.withOpacity(0.7)),
+        name: 'My Orders',
+        route: '/'),
+    AccountListItem(
+        icon: Icon(Icons.favorite_border,
+            size: 30, color: Colors.black.withOpacity(0.7)),
+        name: 'My Favorites',
+        route: '/'),
+    AccountListItem(
+        icon: Icon(Icons.reviews_outlined,
+            size: 30, color: Colors.black.withOpacity(0.7)),
+        name: 'My Reviews',
+        route: '/')
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,29 +46,46 @@ class _ProfileState extends State<Profile> {
                     child: Column(
                       children: [
                         const SizedBox(height: 10),
-                        const Text('My Account',
-                        style: TextStyle(fontWeight: FontWeight.w500,
-                          fontSize: 18.0,),),
-                        const SizedBox(height: 32,),
-                        const CircleAvatar(child: Icon(Icons.account_circle, color:Colors.grey, size: 60,), backgroundColor: Colors.white, radius: 40,),
-                        SizedBox(height: 10,),
+                        const Text(
+                          'My Account',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 18.0,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 32,
+                        ),
+                        const CircleAvatar(
+                          child: Icon(
+                            Icons.account_circle,
+                            color: Colors.grey,
+                            size: 60,
+                          ),
+                          backgroundColor: Colors.white,
+                          radius: 40,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
                         Text('Doğkan Saraç', style: kAppBarTitleTextStyle)
                       ],
                     ),
                   ),
                 ],
               ),
-
             ),
-            Row(
-              children: [
-                Expanded(
-                    child:(Row(children: [
-                      Icon(Icons.favorite_border),
-                      Text('My Favorites')
-                    ],))
-                ),
-              ],
+            Column(
+              children: List.generate(
+                  _itemList.length,
+                  (index) => Row(children: [
+                        Expanded(
+                            child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 24.0, vertical: 14),
+                          child: AccountListItemTile(_itemList[index]),
+                        ))
+                      ])),
             )
           ],
         ),
