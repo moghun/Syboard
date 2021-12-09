ee03963b14e2fce74b8c152abaa08b2dcedf1b8f
Minot1 <dincer.taner123@gmail.com>
2021-12-09 20:55:15 +0300

    added google-login feature

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -11,7 +12,24 @@ class AuthService {
     return _auth.authStateChanges().map(_userFromFirebase);
   }
 
+  Future googleSignIn() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    UserCredential result = await FirebaseAuth.instance.signInWithCredential(credential);
+    User? user = result.user;
+    return _userFromFirebase(user);
+  }
 
   Future signOut() async {
     try {
