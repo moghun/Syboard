37aeb66b7961fc6e537bad7bb0b12b594ec72d45
Minot1 <dincer.taner123@gmail.com>
2021-12-11 13:34:37 +0300

    added crashlytics

--- lib/index.dart
+++ lib/index.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:syboard/utils/color.dart';
