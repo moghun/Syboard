c1789ac91307f0840968109aeb91536a70fe8e74
Minot1 <dincer.taner123@gmail.com>
2021-12-21 15:20:55 +0300

    added tap feature to account list items

--- lib/ui/account_list_item_tile.dart
+++ lib/ui/account_list_item_tile.dart
@@ -1,32 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:syboard/models/account_list_item.dart';
 
-Widget AccountListItemTile(AccountListItem item) {
-  return Column(
-    children: [
-      (Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              item.icon,
-              SizedBox(
-                width: 18,
-              ),
-              Text(
-                item.name,
-                style: TextStyle(
-                    fontWeight: FontWeight.w400, fontSize: 19.0, color: Colors.black.withOpacity(0.7)),
-              ),
-            ],
-          ),
-          Icon(Icons.chevron_right, size: 32,color: Colors.black.withOpacity(0.7))
+Widget AccountListItemTile(AccountListItem item, BuildContext context) {
+  return InkWell(
+    onTap: () {
+      Navigator.pushNamed(context, item.route);
+    },
+    child: Column(
+      children: [
+        const SizedBox(height: 16),
+        (Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                item.icon,
+                SizedBox(
+                  width: 18,
+                ),
+                Text(
+                  item.name,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w400, fontSize: 19.0, color: Colors.black.withOpacity(0.7)),
+                ),
+              ],
+            ),
+            Icon(Icons.chevron_right, size: 32,color: Colors.black.withOpacity(0.7))
 
-        ],
-      )),
-      SizedBox(height: 18),
-      Divider(height: 1, thickness: 1,)
-    ],
+          ],
+        )),
+        SizedBox(height: 16),
+        Divider(height: 1, thickness: 1,)
+      ],
+    ),
   );
 }
