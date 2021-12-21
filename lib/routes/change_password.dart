254d07e2de56ac05b675d66d455a706a70b380a9
Minot1 <dincer.taner123@gmail.com>
2021-12-21 15:59:07 +0300

    added change_password.dart

---
+++ lib/routes/change_password.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:syboard/utils/color.dart';
+
+class ChangePassword extends StatefulWidget {
+  const ChangePassword({Key? key}) : super(key: key);
+
+  @override
+  _ChangePasswordState createState() => _ChangePasswordState();
+}
+
+class _ChangePasswordState extends State<ChangePassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: AppColors.primary,
+        title: const Text("Change Password"),
+      ),
+      body: Container(),
+    );
+  }
+}
