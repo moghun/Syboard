897435b07172eda89c6ec0d0a468b65266e2e507
Muhammed Orhun Gale <89805772+moghun@users.noreply.github.com>
2021-12-26 17:06:23 +0300

    service.dart update for search with queries

--- lib/services/service.dart
+++ lib/services/service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:syboard/models/product.dart';
 
 class Service {
@@ -36,9 +37,6 @@ class Service {
     QuerySnapshot allProductsQuery = await productCollection.get();
     List<Product> list = [];
     allProductsQuery.docs.forEach((doc) {
-      // Map<String, dynamic> productMap = jsonDecode(doc.data().toString());
-      // Product product = Product.fromJson(productMap);
-      // list.add(product);
       list.add(Product(
           imgURL: doc["imgURL"],
           productName: doc["productName"],
@@ -50,4 +48,44 @@ class Service {
     });
     return list;
   }
+
+
+  Future<List<Product>> getSearchResults(query) async {
+
+    List<Product> list = [];
+
+    var products = await FirebaseFirestore.instance.collection('products')
+        .where('productName', isGreaterThanOrEqualTo: query,
+
+      isLessThan: query.substring(0, query.length - 1) +
+          String.fromCharCode(query.codeUnitAt(query.length - 1) + 1),
+
+    ).get();
+
+    products.docs.forEach((doc) => {
+      list.add(Product(
+      imgURL: doc["imgURL"],
+      productName: doc["productName"],
+      rating: doc["rating"],
+      price: doc["price"],
+      seller: doc["seller"],
+      description: doc["description"],
+      onSale: false));
+    });
+
+    return list;
+  }
+
+  FirebaseCrashlytics crashlytics = FirebaseCrashlytics.instance;
+  void doesExist(String src, var results) {
+    bool ch = false;
+    for (int i = 0; i < results.length; i++) {
+      if (src == results[i].identifier ||
+          src == results[i].description) ch = true;
+    }
+    if (ch == false) {
+      crashlytics.setCustomKey("Product not found: ", src);
+    }
+  }
+
 }
