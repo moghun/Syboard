4f7fcb89d2b17610893371e99cbe9daa17f6214e
Dogkan Sarac <dogkansarac@sabanciuniv.edu>
2022-01-09 02:39:00 +0300

    Created order object

---
+++ lib/models/order.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Order {
+  String url;
+  String productName;
+  String pid;
+  num price;
+  num amount;
+  Timestamp purchaseDate;
+
+  Order(
+      {required this.url,
+      required this.productName,
+      required this.pid,
+      required this.price,
+      required this.amount,
+      required this.purchaseDate});
+}
