2b4f991bc75ff7c84bb872e94e426fe798af657d
Minot1 <dincer.taner123@gmail.com>
2022-01-16 15:32:23 +0300

    added buy history and sell history to profile

---
+++ lib/routes/profile/buy_sell_history.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:syboard/models/user_obj.dart';
+import 'package:syboard/utils/styles.dart';
+import 'package:syboard/utils/color.dart';
+import 'package:syboard/views/buy_history.dart';
+import 'package:provider/provider.dart';
+import 'package:syboard/views/sell_history.dart';
+
+class BuySellHistory extends StatefulWidget {
+  const BuySellHistory({Key? key}) : super(key: key);
+
+  @override
+  _BuySellHistoryState createState() => _BuySellHistoryState();
+}
+
+class _BuySellHistoryState extends State<BuySellHistory> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    final user = Provider.of<UserObj?>(context);
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          titleTextStyle: kAppBarTitleTextStyle,
+          toolbarHeight: 55,
+          centerTitle: true,
+          title: const Text("Buy/Sell History"),
+          backgroundColor: AppColors.primary,
+          elevation: 0.0,
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: "Bought"),
+              Tab(text: "Sold"),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [BuyHistory(uid: user!.uid), SellHistory(uid: user.uid)],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/buy_history.dart
@@ -0,0 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:syboard/models/order.dart';
+import 'package:syboard/services/service.dart';
+import 'package:syboard/views/history_card.dart';
+
+class BuyHistory extends StatelessWidget {
+  const BuyHistory({Key? key, required this.uid}) : super(key: key);
+
+  final String uid;
+
+  Future<List<Order>> getOrders() async {
+    List<Order> orders = <Order>[];
+    var buyerRef = Service.userCollection.doc(uid);
+    var o = await Service.ordersCollection
+        .where("buyer", isEqualTo: buyerRef)
+        .get();
+    for (var element in o.docs) {
+      DocumentReference currentProduct = element.get("product");
+      String currentProductName = (await currentProduct.get()).get("productName");
+      num currentProductPrice = (await currentProduct.get()).get("price");
+      String currentProductPicture =
+      (await currentProduct.get()).get("imgURL");
+      String currentPid = currentProduct.id;
+      orders.add(Order(
+          //oid: element.id,
+          //reviewed: element.get("reviewed"),
+          url: currentProductPicture,
+          productName: currentProductName,
+          pid: currentPid,
+          price: currentProductPrice,
+          purchaseDate: element.get("purchaseDate"),
+        amount: element.get("amount")
+      ));
+    }
+    return orders;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: getOrders(),
+        builder: (context, AsyncSnapshot asyncSnapshot) {
+          if (!asyncSnapshot.hasData) return const Text("Loading...");
+          List<Order> orders = asyncSnapshot.data;
+          if (orders.isEmpty) {
+            return const Center(child: Text("No purchase has been made"));
+          }
+          return ListView.builder(
+            scrollDirection: Axis.vertical,
+            itemCount: orders.length,
+            itemBuilder: (context, index) {
+              return HistoryCard(order: orders[index]);
+            },
+          );
+        });
+  }
+}
