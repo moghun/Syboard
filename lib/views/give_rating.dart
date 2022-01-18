63ddca5941ad143626a1c7958eb3d5c6347bb034
EgeAtaCeylan <eata@sabanciuniv.edu>
2022-01-18 23:19:47 +0300

    CREATED GIVE RATING CLASS/VIEW AND ADDED APP BAR

---
+++ lib/views/give_rating.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:syboard/utils/styles.dart';
+import 'package:syboard/utils/color.dart';
+import 'package:syboard/models/order.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+class GiveRating extends StatefulWidget {
+  const GiveRating ({Key? key, required this.order}) : super(key: key);
+
+  final Order order;
+
+  @override
+  State<GiveRating> createState() => _GiveRatingState();
+}
+
+class _GiveRatingState extends State<GiveRating> {
+  num rating = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/notifications');
+              },
+              icon: const Icon(
+                Icons.notifications_outlined,
+                color: Colors.black54,
+                size: 28,
+              )),
+          const SizedBox(width: 8)
+        ],
+        title: Row(
+          children: [
+            const SizedBox(width: 8),
+            SizedBox(
+                width: 38,
+                height: 48,
+                child: Image.asset('lib/images/logo_small.png')),
+            const SizedBox(width: 8),
+            Text(
+              'Syboard',
+              style: kHeadingTextStyle,
+            ),
+          ],
+        ),
+        backgroundColor: Colors.white,
+        elevation: 2.0,
+      ),
+    );
+  }
+}
