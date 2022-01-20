3d608c560a44fb3a241a70ad8617f4c2df44aeed
EgeAtaCeylan <eata@sabanciuniv.edu>
2022-01-20 22:50:28 +0300

    CREATED COMMENT PRODUCT CLASS/VIEW AND ADDED APP BAR

---
+++ lib/views/comment_product.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:syboard/utils/styles.dart';
+import 'package:syboard/utils/color.dart';
+import 'package:syboard/models/order.dart';
+
+class CommentProduct extends StatefulWidget {
+  const CommentProduct({Key? key, required this.order}) : super(key: key);
+
+  final Order order;
+
+  @override
+  State<CommentProduct> createState() => _CommentProductState();
+}
+
+class _CommentProductState extends State<CommentProduct> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/notifications');
+              },
+              icon: const Icon(
+                Icons.notifications_outlined,
+                color: Colors.black54,
+                size: 28,
+              )),
+          const SizedBox(width: 8)
+        ],
+        title: Row(
+          children: [
+            const SizedBox(width: 8),
+            SizedBox(
+                width: 38,
+                height: 48,
+                child: Image.asset('lib/images/logo_small.png')),
+            const SizedBox(width: 8),
+            Text(
+              'Syboard',
+              style: kHeadingTextStyle,
+            ),
+          ],
+        ),
+        backgroundColor: Colors.white,
+        elevation: 2.0,
+      ),
+    );
+  }
+}
