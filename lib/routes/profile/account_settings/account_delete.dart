4bed2bc9709c7e9a8cbbb10d3284aabe5e91f1e1
mizbahc <m.mzbh@outlook.com>
2022-01-23 21:39:32 +0300

    delete account

--- lib/routes/profile/account_settings/account_delete.dart
+++ lib/routes/profile/account_settings/account_delete.dart
@@ -6,61 +6,66 @@ import 'package:syboard/services/auth.dart';
 import 'package:syboard/utils/color.dart';
 import 'package:syboard/utils/styles.dart';
 
-class AccountDeletePP extends StatelessWidget{
-   const AccountDeletePP({
+class AccountDeletePP extends StatefulWidget {
+  const AccountDeletePP({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<AccountDeletePP> createState() => _AccountDeletePPState();
+}
 
+class _AccountDeletePPState extends State<AccountDeletePP> {
+  String pass = "";
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<UserObj?>(context, listen: false);
     return SafeArea(
-      child: Material( 
-       
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-              //  SizedBox(height: 20,),
-              //   OutlinedButton(
-              //     onPressed: () async {
-                    
-              //     },
-              //     child: Padding(
-              //       padding: EdgeInsets.all(8.0),
-              //       child: Text(
-              //         'Disable Account',
-              //         style: kButtonDarkTextStyle,
-              //       ),
-              //     ),
-              //     style: OutlinedButton.styleFrom(
-              //       backgroundColor: AppColors.primary,
-              //     ),
-              //   ),
-              SizedBox(height: 20,),
-              OutlinedButton(
-              onPressed: () async {
-                await AuthService().deleteAccount(user!.uid);
-                Navigator.of(context).pop();
-                Navigator.of(context).pop();
-
-              },
-              child: Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  'Delete Account',
-                  style: kButtonDarkTextStyle,
-                ),
-              ),
-              style: OutlinedButton.styleFrom(
-                backgroundColor: Colors.red,
+      child: Material(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+           SizedBox(height: 20,),
+          
+          Row(
+            children: [
+              const Text(
+                  "Please confirm your identity by providing your password"),
+              const SizedBox(height: 10),
+              TextField(
+                  obscureText: true,
+                  autocorrect: false,
+                  onChanged: (value) {
+                    pass = value;
+                  },
+                  decoration: const InputDecoration(hintText: "Your Password"))
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          OutlinedButton(
+            onPressed: () async {
+              if(pass != ""){
+                
+              }
+              await AuthService().deleteAccount(user!.uid);
+              Navigator.of(context).pop();
+              Navigator.of(context).pop();
+            },
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Delete Account',
+                style: kButtonDarkTextStyle,
               ),
             ),
-                
-                 
-              ],
-            )),
-      
+            style: OutlinedButton.styleFrom(
+              backgroundColor: Colors.red,
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
